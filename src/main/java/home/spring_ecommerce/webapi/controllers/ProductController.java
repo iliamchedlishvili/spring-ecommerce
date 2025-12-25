9b28a0e1bbe96559766a110341b17388e34ee0cc
ilia mchedlishvili <ilia.mchedlishvili@gmail.com>
2025-12-25 13:35:23 +0400

    added ProductController

---
+++ src/main/java/home/spring_ecommerce/webapi/controllers/ProductController.java
@@ -0,0 +1,10 @@
+package home.spring_ecommerce.webapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+}
