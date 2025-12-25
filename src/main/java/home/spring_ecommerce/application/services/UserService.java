13d793be942ba39ef6626113403448420bf7b7da
ilia mchedlishvili <ilia.mchedlishvili@gmail.com>
2025-12-25 10:11:39 +0400

    user management crud

--- src/main/java/home/spring_ecommerce/application/services/UserService.java
+++ src/main/java/home/spring_ecommerce/application/services/UserService.java
@@ -15,7 +15,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
--- src/main/java/home/spring_ecommerce/webapi/controllers/UserController.java
+++ src/main/java/home/spring_ecommerce/webapi/controllers/UserController.java
@@ -15,8 +15,6 @@ public class UserController {
 
     private final UserService userService;
 
-
-
     public UserController(UserService userService) {
         this.userService = userService;
     }
